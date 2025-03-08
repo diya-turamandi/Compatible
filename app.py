5510208b692a037ddc84dd9b260a5746fd75e0ba
Adi1614 <adityasingh141606@gmail.com>
2025-03-08 21:10:42 +0530

    Routes established and made buttons

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from pymongo import MongoClient
-from flask import Flask, render_template, flash, url_for
+from flask import Flask, render_template, flash, url_for, request, session, redirect
 from calc import compCompatibility
 
 
 app = Flask(__name__, template_folder='templates')
+
+app.secret_key = "mfl"
 
 
 client = MongoClient(
@@ -20,18 +22,48 @@ def index():
     return render_template("pyaar.html")
 
 
-@app.route('/calc', methods=["POST"])
+@app.route('/lovemeter', methods=["GET", "POST"])
+def lovemeter():
+    percentage = request.args.get('perc')
+
+    if request.method == "POST":
+        name1 = request.form.get('name1')
+        name2 = request.form.get('name2')
+        print("lovemeter", name1, name2)
+        return redirect(url_for('calculate', name1=name1, name2=name2))
+    
+    return render_template("lovemeter.html", perc=percentage)
+
+
+
+@app.route('/astromath')
+def astromath():
+    return render_template("astromath.html")
+
+
+
+@app.route('/calc')
 def calculate():
     # name1 and name2 will take from form
-    name1 = ""
-    name2 = ""
+    name1 = request.args.get('name1')  # Get from query parameters
+    name2 = request.args.get('name2')
 
+    print(name1, name2)
+    if not name1 or not name2:
+        flash("Please enter both names!", "error")
+        return redirect(url_for('lovemeter'))
+    
     try:
-        comp = compCompatibility(name1, name2)
+        percentage = compCompatibility(name1, name2)
+        name_collection.insert_one({"Name1": name1,
+                                    "Name2": name2,
+                                    "Percentage": percentage})
+        return redirect(url_for('lovemeter', perc=percentage))
+
 
     except ValueError:
         flash("Oops!! Make sure you spell the Names correctly with no special characters!!", "error")
-        return render_template("pyaar.html")
+        return redirect(url_for('lovemeter'))
 
 
 
